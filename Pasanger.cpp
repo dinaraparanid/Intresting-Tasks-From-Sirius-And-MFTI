9d0908e3ae1a09b9179a84267bc09f6ef9d1659f
Paranid5 <58735614+dinaraparanid@users.noreply.github.com>
2020-10-08 08:31:36 +0300

    Update Pasanger.cpp

--- Pasanger.cpp
+++ Pasanger.cpp
@@ -33,23 +33,22 @@ s 1 5
 #include <algorithm>
 #include <vector>
 
-enum STOrder
+/*enum STOrder
 {
 	MAX,
 	MIN
-};
+};*/
 
-template <typename T>
 class SegmentTree
 {
  private:
-	size_t last_; // размер
-	std::vector<T> builder_; // то, где поддерживаем rmq
-	std::vector<T> constructor_; // первоначальный вектор
-	STOrder order_;
+	int last_; // размер
+	std::vector<int> builder_; // то, где поддерживаем rmq
+	std::vector<int> constructor_; // первоначальный вектор
+	//STOrder order_;
 
-	static inline T left(T _p) { return _p << 1; } // левая ветвь
-	static inline T right(T _p) { return (_p << 1) + 1; }  // правая ветвь
+	static inline int left(int _p) { return _p << 1; } // левая ветвь
+	static inline int right(int _p) { return (_p << 1) + 1; }  // правая ветвь
 
 	/**
 	Построение дерева отрезков (сегментов).
@@ -58,7 +57,7 @@ class SegmentTree
 	@param _r - текущий индекс вправо
 	*/
 
-	void build (T _p, size_t _l, size_t _r)
+	void build (int _p, int _l, int _r)
 	{
 		if (_l == _r)
 			builder_[_p] = _l; // дошли до листа
@@ -68,14 +67,14 @@ class SegmentTree
 			build(right(_p), (_l + _r) / 2 + 1, _r); // строим правую ветвь
 
 			// считаем rmq для права и лева
-			const T p1 = builder_[left(_p)];
-			const T p2 = builder_[right(_p)];
+			const int p1 = builder_[left(_p)];
+			const int p2 = builder_[right(_p)];
 
 			// выбираем лучший результат
-			if (order_ == MAX)
-				builder_[_p] = (constructor_[p1] >= constructor_[p2] ? p1 : p2);
-			else
-				builder_[_p] = (constructor_[p1] <= constructor_[p2] ? p1 : p2);
+			//if (order_ == MAX)
+            builder_[_p] = (constructor_[p1] >= constructor_[p2] ? p1 : p2);
+			/*else
+				builder_[_p] = (constructor_[p1] <= constructor_[p2] ? p1 : p2);*/
 		}
 	}
 
@@ -87,7 +86,7 @@ class SegmentTree
 	@return rmq(_i, _j) в ЧАСТИ дерева
 	*/
 
-	T rmq (T _p, size_t _l, size_t _r, size_t _i, size_t _j)
+	int rmq (int _p, int _l, int _r, int _i, int _j)
 	{
 		if (_i > _r || _j < _l)  // если вышли за границу
 			return -1;
@@ -96,17 +95,17 @@ class SegmentTree
 			return builder_[_p];
 
 		// считаем rmq справа и слева
-		const T p1 = rmq(left(_p), _l, (_l + _r) / 2, _i, _j);
-		const T p2 = rmq(right(_p), (_l + _r) / 2 + 1, _r, _i, _j);
+		const int p1 = rmq(left(_p), _l, (_l + _r) / 2, _i, _j);
+		const int p2 = rmq(right(_p), (_l + _r) / 2 + 1, _r, _i, _j);
 
 		if (p1 == -1) return p2;
 		if (p2 == -1) return p1;
 
 		// выбираем лучший результат
-		if (order_ == MAX)
-			return (constructor_[p1] >= constructor_[p2] ? p1 : p2);
-		else
-			return (constructor_[p1] <= constructor_[p2] ? p1 : p2);
+		//if (order_ == MAX)
+        return (constructor_[p1] >= constructor_[p2] ? p1 : p2);
+		/*else
+			return (constructor_[p1] <= constructor_[p2] ? p1 : p2);*/
 	}
 
  public:
@@ -116,11 +115,11 @@ class SegmentTree
 	@param _vec - вектор, для которого нужно найти rmq
 	*/
 
-	explicit SegmentTree(const std::vector<T>& _vec, STOrder _order)
+	explicit SegmentTree(const std::vector<int>& _vec/*, STOrder _order*/)
 	{
 		constructor_ = _vec;
 		last_ = _vec.size();
-		order_ = _order;
+		//order_ = _order;
 
 		builder_.assign(4 * last_, 0);
 		build(1, 0, last_ - 1);
@@ -131,14 +130,14 @@ class SegmentTree
 	@param _vec - новый вектор
 	*/
 
-	inline void rebuild (const std::vector<T>& _vec)
+	inline void rebuild (const std::vector<int>& _vec)
 	{
 		constructor_ = _vec;
-		std::for_each(builder_.begin(), builder_.end(), [](T& _elem) { _elem = 0; });
+		std::for_each(builder_.begin(), builder_.end(), [](int& _elem) { _elem = 0; });
 		build(1, 0, last_ - 1);
 	}
 
-	inline T rmq (size_t _i, size_t _j) { return rmq(1, 0, last_ - 1, _i, _j); } // перегрузка для нахождения
+	inline int rmq (int _i, int _j) { return rmq(1, 0, last_ - 1, _i, _j); } // перегрузка для нахождения
 	// rmq(_i, _j) для всего дерева
 };
 
@@ -162,7 +161,9 @@ int main()
 			pref[i] = pn[i] + (i > 0 ? pref[i - 1] : 0);
 		}
 
-		SegmentTree<int> tree(pn, MAX);
+		SegmentTree tree(pn);
+
+		bool rebuilded = true;
 
 		for (int i = 0; i < m; i++)
 		{
@@ -172,21 +173,37 @@ int main()
 			switch (act)
 			{
 			case 'm':
+			    if (!rebuilded)
+                {
+                    for (int q = x; q < n; q++)
+                        pref[q] = pn[q] + (q > 0 ? pref[q - 1] : 0);
+
+                    tree.rebuild(pn);
+
+                    rebuilded = true;
+                }
+
 				std::printf("%d\n", pn[tree.rmq(x, y + 1)]);
 				break;
 			case 's':
+			    if (!rebuilded)
+                {
+                    for (int q = x; q < n; q++)
+                        pref[q] = pn[q] + (q > 0 ? pref[q - 1] : 0);
+
+                    tree.rebuild(pn);
+
+                    rebuilded = true;
+                }
+
 				std::printf("%llu\n", pref[y] - (x > 0 ? pref[x - 1] : 0));
 				break;
 			default:
+			    rebuilded = false;
 				std::scanf("%d", &t);
 
 				for (int ptr = x; ptr <= y; ptr++)
 					pn[ptr] = std::min(pn[ptr], t);
-
-				for (int q = x; q < n; q++)
-					pref[q] = pn[q] + (q > 0 ? pref[q - 1] : 0);
-
-				tree.rebuild(pn);
 			}
 		}
 	}
